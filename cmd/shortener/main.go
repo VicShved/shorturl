2ed35e61db95dbf249cb57f960c01f134517fdf5
Viktor Shvedov <viktor.shvedov@piklema.com>
2025-02-24 11:31:17 +0500

    iter2 - start

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,84 +2,15 @@ package main
 
 import (
 	"fmt"
-	"hash/fnv"
+	"github.com/VicShved/shorturl/internal/app"
 	"io"
 	"net/http"
-	"strconv"
 	"strings"
 )
 
 var urlmap map[string]string
 
-func hash(s string) string {
-	h := fnv.New32a()
-	_, err := h.Write([]byte(s))
-	if err != nil {
-		return ""
-	}
-	return strconv.Itoa(int(h.Sum32()))
-}
-
-func handle(w http.ResponseWriter, r *http.Request) {
-
-	good1 := (r.Method != http.MethodPost)
-	good2 := (r.Method != http.MethodGet)
-	good := good1 && good2
-	if good {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	//if r.Header.Get("Content-Type") != "text/plain" {
-	//	w.WriteHeader(http.StatusBadRequest)
-	//	return
-	//}
-
-	w.Header().Set("Content-Type", "text/plain")
-
-	if r.Method == http.MethodPost {
-		urlBytes, _ := io.ReadAll(r.Body)
-		fmt.Println("string(urlBytes) = ", string(urlBytes))
-		//if err != nil {
-		//	w.WriteHeader(http.StatusBadRequest)
-		//	return
-		//}
-		//if len(urlBytes) == 0 {
-		//	w.WriteHeader(http.StatusBadRequest)
-		//	return
-		//}
-		key := hash(string(urlBytes))
-		urlmap[key] = string(urlBytes)
-		w.WriteHeader(http.StatusCreated)
-		newurl := "http://localhost:8080/" + key
-		fmt.Println("newurl = ", newurl)
-		w.Write([]byte(newurl))
-		return
-	}
-
-	if r.Method == http.MethodGet {
-		urlstr := strings.TrimPrefix(r.URL.Path, "/")
-		fmt.Println("urlstr =", urlstr)
-		//if len(urlstr) == 0 {
-		//	w.WriteHeader(http.StatusBadRequest)
-		//	return
-		//}
-
-		url, exists := urlmap[urlstr]
-		fmt.Println("exists = ", exists)
-
-		if !exists {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		fmt.Println("url = ", url)
-		w.Header().Set("Location", url)
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	}
-}
-
-func handlePOST(w http.ResponseWriter, r *http.Request) {
+func HandlePOST(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -102,7 +33,7 @@ func handlePOST(w http.ResponseWriter, r *http.Request) {
 	//	w.WriteHeader(http.StatusBadRequest)
 	//	return
 	//}
-	key := hash(string(urlBytes))
+	key := app.Hash(string(urlBytes))
 	urlmap[key] = string(urlBytes)
 	w.WriteHeader(http.StatusCreated)
 	newurl := "http://localhost:8080/" + key
@@ -110,7 +41,7 @@ func handlePOST(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(newurl))
 }
 
-func handleGET(w http.ResponseWriter, r *http.Request) {
+func HandleGET(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -139,8 +70,8 @@ func handleGET(w http.ResponseWriter, r *http.Request) {
 func main() {
 	urlmap = make(map[string]string)
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /", handlePOST)
-	mux.HandleFunc("GET /", handleGET)
+	mux.HandleFunc("POST /", HandlePOST)
+	mux.HandleFunc("GET /", HandleGET)
 	fmt.Println("Listening on :8080")
 	err := http.ListenAndServe(`localhost:8080`, mux)
 	if err != nil {
