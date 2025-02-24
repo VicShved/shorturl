01a8d7716aa4c8f14f7e85adb1952eb14536f730
Viktor Shvedov <viktor.shvedov@piklema.com>
2025-02-24 10:52:41 +0500

    iter1 - vet

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -79,10 +79,69 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlePOST(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//if r.Header.Get("Content-Type") != "text/plain" {
+	//	w.WriteHeader(http.StatusBadRequest)
+	//	return
+	//}
+
+	w.Header().Set("Content-Type", "text/plain")
+
+	urlBytes, _ := io.ReadAll(r.Body)
+	fmt.Println("string(urlBytes) = ", string(urlBytes))
+	//if err != nil {
+	//	w.WriteHeader(http.StatusBadRequest)
+	//	return
+	//}
+	//if len(urlBytes) == 0 {
+	//	w.WriteHeader(http.StatusBadRequest)
+	//	return
+	//}
+	key := hash(string(urlBytes))
+	urlmap[key] = string(urlBytes)
+	w.WriteHeader(http.StatusCreated)
+	newurl := "http://localhost:8080/" + key
+	fmt.Println("newurl = ", newurl)
+	w.Write([]byte(newurl))
+	return
+}
+
+func handleGET(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	urlstr := strings.TrimPrefix(r.URL.Path, "/")
+	fmt.Println("urlstr =", urlstr)
+	//if len(urlstr) == 0 {
+	//	w.WriteHeader(http.StatusBadRequest)
+	//	return
+	//}
+
+	url, exists := urlmap[urlstr]
+	fmt.Println("exists = ", exists)
+
+	if !exists {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("url = ", url)
+	w.Header().Set("Location", url)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
 func main() {
 	urlmap = make(map[string]string)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handle)
+	mux.HandleFunc("POST /", handlePOST)
+	mux.HandleFunc("GET /", handleGET)
 	fmt.Println("Listening on :8080")
 	err := http.ListenAndServe(`localhost:8080`, mux)
 	if err != nil {
