86cb17cf743a95dfc169b8c866082f66f6913402
Viktor Shvedov <viktor.shvedov@piklema.com>
2025-03-14 22:18:16 +0300

    iter6 - wip

---
+++ internal/logger/logger.go
@@ -0,0 +1,72 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"net/http"
+	"time"
+	//"net/http"
+)
+
+var Log *zap.Logger = zap.NewNop()
+
+func Initialize(level string) error {
+	lvl, err := zap.ParseAtomicLevel(level)
+	if err != nil {
+		return err
+	}
+	cfg := zap.NewProductionConfig()
+	cfg.Level = lvl
+	log, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+	Log = log
+	defer Log.Sync()
+	return nil
+}
+
+type responseData struct {
+	status int
+	size   int
+}
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size = size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(status int) {
+	r.ResponseWriter.WriteHeader(status)
+	r.responseData.status = status
+}
+
+func AddLogging(handler http.Handler) http.Handler {
+
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+		handler.ServeHTTP(&lw, r)
+		duration := time.Since(start)
+		Log.Info("",
+			zap.String("url", r.RequestURI),
+			zap.String("method", r.Method),
+			zap.Duration("duration", duration),
+			zap.Int("status", responseData.status),
+			zap.Int("size", responseData.size),
+		)
+
+	}
+	return http.HandlerFunc(logFn)
+}
