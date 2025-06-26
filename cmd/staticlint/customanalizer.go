8f5c072183792909788c9f944f29290d8a26e621
Viktor Shvedov <viktor.shvedov@piklema.com>
2025-06-26 13:32:32 +0300

    iter19

--- cmd/staticlint/customanalizer.go
+++ cmd/staticlint/customanalizer.go
@@ -2,6 +2,7 @@ package main
 
 import "golang.org/x/tools/go/analysis"
 
+// Анализатор на os.Exit
 var ErrCheckAnalyzer = &analysis.Analyzer{
 	Name: "osExitCheck",
 	Doc:  "check os.Exit in func main package main",
