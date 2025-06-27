47d16c5ea554f3fbe90d5124149cf0526808dce6
Viktor Shvedov <viktor.shvedov@piklema.com>
2025-06-27 16:43:39 +0300

    iter20

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/VicShved/shorturl/internal/app"
 	"github.com/VicShved/shorturl/internal/logger"
 	"github.com/VicShved/shorturl/internal/server"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
 func main() {
+	fmt.Print("Build version: ", buildVersion)
+	fmt.Print("Build date: ", buildDate)
+	fmt.Print("Build commit: ", buildCommit)
 	// Get app config
 	var config = app.GetServerConfig()
 	// Init custom logger
