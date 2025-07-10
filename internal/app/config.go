047097840aaf91fa72af2ad97a5ba27916f64021
Viktor Shvedov <viktor.shvedov@piklema.com>
2025-07-10 09:31:49 +0300

    wip

--- internal/app/config.go
+++ internal/app/config.go
@@ -25,7 +25,7 @@ func GetServerConfig() *ServerConfigStruct {
 	flag.StringVar(&ServerConfig.ServerAddress, "a", "localhost:8080", "start base url")
 	flag.StringVar(&ServerConfig.BaseURL, "b", "http://localhost:8080", "result base url")
 	flag.StringVar(&ServerConfig.FileStoragePath, "f", "", "file storage path")
-	flag.BoolVar(&ServerConfig.EnableHTTPS, "s", true, "enable https")
+	flag.BoolVar(&ServerConfig.EnableHTTPS, "s", false, "enable https")
 	flag.StringVar(&ServerConfig.DBDSN, "d", "", "DataBase DSN")
 	flag.StringVar(&ServerConfig.SecretKey, "s", "VeryImpotantSecretKey.YesYes", "Secret key")
 	flag.StringVar(&ServerConfig.LogLevel, "l", "INFO", "Log level")
