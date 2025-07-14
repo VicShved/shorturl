aaf908c2a04551ecb4d77166781bc9d8f9af7a59
Viktor Shvedov <viktor.shvedov@piklema.com>
2025-07-14 18:45:54 +0300

    iter23 add test

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -1,1 +1,40 @@
 package app
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestUpdateConfig
+func TestUpdateConfig(t *testing.T) {
+	tests := []struct {
+		target ServerConfigStruct
+		source ServerConfigStruct
+		want   ServerConfigStruct
+	}{
+		{
+			target: ServerConfigStruct{ServerAddress: ""},
+			source: ServerConfigStruct{ServerAddress: "source"},
+			want:   ServerConfigStruct{ServerAddress: "source"},
+		},
+		{
+			target: ServerConfigStruct{ServerAddress: "target"},
+			source: ServerConfigStruct{ServerAddress: "source"},
+			want:   ServerConfigStruct{ServerAddress: "target"},
+		},
+		{
+			target: ServerConfigStruct{ServerAddress: "target"},
+			source: ServerConfigStruct{ServerAddress: ""},
+			want:   ServerConfigStruct{ServerAddress: "target"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			result := updateConfig(&test.target, &test.source)
+			assert.Equal(t, test.want, *result)
+		},
+		)
+	}
+}
